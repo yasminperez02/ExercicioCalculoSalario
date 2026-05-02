4451e40e612801de7649077b6200e4edd28bd7c6
yasminperez02 <yasminrafaelaramires@gmail.com>
2026-05-02 20:20:39 -0300

    finalização do programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Employees;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,5 +10,22 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Employees employee = new Employees();
+
+        System.out.println("Name: ");
+        employee.name = sc.nextLine();
+        System.out.println("Gross Salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println("Employee: " + employee.name + ", $" + employee.netSalary());
+
+        System.out.println("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println("Update data: " + employee);
+        sc.close();
     }
 }
